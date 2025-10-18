eb0f27ad73255caffb58c258cbbd2d31bc5030a6
mondem0 <138859687+mondem0@users.noreply.github.com>
2025-10-19 00:10:02 +0200

    Update EnemyConfigs.lua

--- ReplicatedStorage/Modules/Config/EnemyConfigs.lua
+++ ReplicatedStorage/Modules/Config/EnemyConfigs.lua
@@ -1,25 +1,53 @@
 local EnemyConfigs = {
-    Grunt = {
-        Name = "Grunt",
-        ModelName = "Grunt",
-        Health = 60,
-        Speed = 12,
-        Reward = 15
-    },
-    Runner = {
-        Name = "Runner",
-        ModelName = "Runner",
-        Health = 45,
-        Speed = 18,
-        Reward = 12
-    },
-    Tank = {
-        Name = "Tank",
-        ModelName = "Tank",
-        Health = 160,
-        Speed = 8,
-        Reward = 35
-    }
+	Grunt = {
+		Name = "Grunt",
+		ModelName = "Grunt",
+		Health = 45,
+		Speed = 8,
+		Reward = 15
+	},
+	Runner = {
+		Name = "Runner",
+		ModelName = "Runner",
+		Health = 25,
+		Speed = 10,
+		Reward = 12
+	},
+	Tank = {
+		Name = "Tank",
+		ModelName = "Tank",
+		Health = 350,
+		Speed = 6,
+		Reward = 35
+	},
+	Shielder = {
+		Name = "Shielder",
+		ModelName = "Shielder",
+		Health = 650,
+		Speed = 5,
+		Reward = 40
+	},
+	Boss1 = {
+		Name = "Boss1",
+		ModelName = "Boss1",
+		Health = 8000,
+		Speed = 3,
+		Reward = 100
+	},
+	Lightning = {
+		Name = "Lightning",
+		ModelName = "Lightning",
+		Health = 120,
+		Speed = 27,
+		Reward = 50
+	},
+	LightningBoss = {
+		Name = "LightningBoss",
+		ModelName = "LightningBoss",
+		Health = 5500,
+		Speed = 13,
+		Reward = 200
+	}
 }
 
 return EnemyConfigs
