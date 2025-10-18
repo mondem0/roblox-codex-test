96ef56c3421561b42545c169368aa49c7c182f47
mondem0 <138859687+mondem0@users.noreply.github.com>
2025-10-18 22:16:42 +0200

    feat: support mixed enemy spawns

--- ReplicatedStorage/Modules/Config/WaveConfigs.lua
+++ ReplicatedStorage/Modules/Config/WaveConfigs.lua
@@ -5,18 +5,46 @@ local WaveConfigs = {
     {
         { Type = "Grunt", Count = 14, Delay = 0.7 },
         { Type = "Runner", Count = 6, Delay = 0.6 },
+        {
+            Spawns = {
+                { Type = "Grunt", Count = 2 },
+                { Type = "Runner", Count = 1 },
+            },
+            Repeat = 4,
+            Delay = 0.65,
+        },
     },
     {
-        { Type = "Runner", Count = 14, Delay = 0.55 },
+        {
+            Spawns = {
+                { Type = "Runner", Count = 2 },
+                { Type = "Grunt", Count = 1 },
+            },
+            Repeat = 6,
+            Delay = 0.55,
+        },
         { Type = "Grunt", Count = 16, Delay = 0.65 },
     },
     {
         { Type = "Tank", Count = 6, Delay = 1.3 },
-        { Type = "Runner", Count = 10, Delay = 0.6 },
+        {
+            Spawns = {
+                { Type = "Runner", Count = 2 },
+                { Type = "Grunt", Count = 1 },
+            },
+            Repeat = 5,
+            Delay = 0.6,
+        },
     },
     {
-        { Type = "Grunt", Count = 20, Delay = 0.6 },
-        { Type = "Tank", Count = 8, Delay = 1.1 },
+        {
+            Spawns = {
+                { Type = "Grunt", Count = 2 },
+                { Type = "Tank", Count = 1 },
+            },
+            Repeat = 6,
+            Delay = 0.7,
+        },
         { Type = "Runner", Count = 12, Delay = 0.55 },
     },
 }
